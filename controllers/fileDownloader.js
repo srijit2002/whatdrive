ad8432e69af5ff7a06be73761153196c2c377037
Srijit <srijitm906@gmail.com>
2022-02-06 11:08:30 +0530

    fix: add error handler

--- controllers/fileDownloader.js
+++ controllers/fileDownloader.js
@@ -3,13 +3,18 @@ const path = require("path");
 const fs = require("fs");
 
 const downloadFile = async (url, filename) => {
-  const filepath = path.join(__dirname, "../", "public", filename);
-  const response = await axios({
-    method: "get",
-    url,
-    responseType: "stream",
-  });
-  response.data.pipe(fs.createWriteStream(filepath));
+  try {
+    const filepath = path.join(__dirname, "../", "public", filename);
+    const response = await axios({
+      method: "get",
+      url,
+      responseType: "stream",
+    });
+    await response.data.pipe(fs.createWriteStream(filepath));
+  } catch (error) {
+    console.log(error)
+  }
+ 
 };
 // downloadFile(
 //   "https://api.twilio.com/2010-04-01/Accounts/ACc1986d61d74782c7e657dd7f38f8dcda/Messages/MMb134929c9a8ccf2579af280bedf51968/Media/ME093599dc64b10c2b0d7210c5b0f15e02",
