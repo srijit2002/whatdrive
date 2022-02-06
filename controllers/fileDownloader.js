ffd779e85fe6a7ee684aef2228edf43f07550af4
Srijit <srijitm906@gmail.com>
2022-02-06 12:43:02 +0530

    fix: fix minor bugs in filedownloader

--- controllers/fileDownloader.js
+++ controllers/fileDownloader.js
@@ -1,23 +1,21 @@
-const axios = require("axios");
-const path = require("path");
-const fs = require("fs");
+const Fs = require('fs')  
+const Axios = require('axios')
 
-const downloadFile = async (url, filename) => {
-  try {
-    const filepath = path.join(__dirname, "../", "public", filename);
-    const response = await axios({
-      method: "get",
-      url,
-      responseType: "stream",
-    });
-    await response.data.pipe(fs.createWriteStream(filepath));
-  } catch (error) {
-    console.log(error)
-  }
- 
-};
-// downloadFile(
-//   "https://api.twilio.com/2010-04-01/Accounts/ACc1986d61d74782c7e657dd7f38f8dcda/Messages/MMb134929c9a8ccf2579af280bedf51968/Media/ME093599dc64b10c2b0d7210c5b0f15e02",
-//   "image/jpeg"
-// );
+async function downloadFile (url,path) {
+  const writer = Fs.createWriteStream(path)
+
+  const response = await Axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
+}
+
 module.exports = downloadFile;
--- routes/route.js
+++ routes/route.js
@@ -1,29 +1,26 @@
-const express=require("express");
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const driveInstance=require("../vendors/auth");
+const driveInstance = require("../vendors/auth");
 
 //controllers
 const uploadFile = require("../controllers/fileUploader");
 const downloadFile = require("../controllers/fileDownloader");
-
+const fs = require("fs");
 
 //initializing api route
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post("/upload",async (req, res) => {
-  try {
-    const {url,filetype}=req.body;
-    const filename = `${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}.${path.basename(filetype)}`;
-    await downloadFile(url, filename);
-    const localfilepath = path.join(__dirname,"../","public",filename);
-    await uploadFile(localfilepath, filetype, filename, driveInstance);
+app.post("/upload", async (req, res) => {
+  const { url, filetype } = req.body;
+  const filename = `${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}.${path.basename(filetype)}`;
+  const localfilepath = path.join(__dirname, "../", "public", filename);
+  await downloadFile(url,localfilepath);
+  await uploadFile(localfilepath,filetype,filename,driveInstance);
+  if(fs.existsSync(localfilepath)){
     fs.unlinkSync(localfilepath);
-    res.status(200).json({message:`file uploaded to your drive succesfully`});
-  } catch (error) {
-    res.status(500).json({ message: `file upload failed.\n error-message:${error}` });
   }
+  res.json({message:"file saved in your drive successfully"});
 });
-module.exports=app;
+module.exports = app;
